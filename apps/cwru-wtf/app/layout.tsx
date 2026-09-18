d0689f3d748465f48d7bf8ce64644b557d658a5d
ignasxv <62844231+ignasxv@users.noreply.github.com>
2026-09-17 23:03:45 -0400

    Introduce the CWRU.WTF spark identity

--- apps/cwru-wtf/app/layout.tsx
+++ apps/cwru-wtf/app/layout.tsx
@@ -7,29 +7,18 @@ import { Toaster } from "sonner"
 import { SessionProvider } from "next-auth/react"
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://cwru.wtf"),
   title: "CWRU.WTF - We Tinker Fearlessly",
   description: "A collective of CWRU students building the future (or just building cool stuff).",
-  icons: {
-    icon: '/favicon.svg',
-  },
   openGraph: {
     title: "CWRU.WTF - We Tinker Fearlessly",
     description: "A collective of CWRU students building the future (or just building cool stuff).",
-    images: [
-      {
-        url: '/cwru-wtf.png',
-        width: 1200,
-        height: 630,
-        alt: 'CWRU.WTF - We Tinker Fearlessly',
-      },
-    ],
     type: 'website',
   },
   twitter: {
     card: 'summary_large_image',
     title: "CWRU.WTF - We Tinker Fearlessly",
     description: "A collective of CWRU students building the future (or just building cool stuff).",
-    images: ['/cwru-wtf.png'],
   },
 }
 
